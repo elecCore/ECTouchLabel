3f35a7860e359a06b61dc7afdb87cae4bd58bf77
刘超09068 <lc09068@ly.com>
2016-06-06 19:26:40 +0800

    Revert "测试"
    This reverts commit c5aace4a0843a70c40b6ca3720d6f99d6db55ea3.

--- ECTouchLabel/ViewController.m
+++ ECTouchLabel/ViewController.m
@@ -32,7 +32,9 @@
                           cancelButtonTitle:@"确定"
                             otherButtonTitles:nil] show];
     };
-
+    
+    NSString *test4;
+    
     [testLableView sizeToFitWithMaxSize:CGSizeMake(testLableView.frame.size.width, MAXFLOAT)];
 }
 
